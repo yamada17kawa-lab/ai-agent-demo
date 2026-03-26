f00bdb88aacb393842203baf361b4187f90f4db7
YangWangWang <yamada17kawa@gmail.com>
2026-03-26 09:46:28 +0800

    向量化的基础用法

--- TestEmbedding.py
+++ TestEmbedding.py
@@ -2,5 +2,6 @@ from langchain_ollama import OllamaEmbeddings
 
 embed = OllamaEmbeddings(model="qwen3-embedding:4b")
 
-
+print(embed.embed_query("我喜欢你"))
+print(embed.embed_documents(["我love你", "我中意你", "我嘿混你"]))
 
