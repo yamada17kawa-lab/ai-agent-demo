4ac6eb63e36a8ba0ef8acb27e7a15173a8cb61a2
YangWangWang <yamada17kawa@gmail.com>
2026-03-28 12:38:44 +0800

    aaa

--- rag/App.py
+++ rag/App.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.title("上传文件")
+
+file = st.file_uploader(
+    "上传文件",
+    type="text/plain",
+    accept_multiple_files=False,
+)
+
+if file is not None:
+    fname = file.name
+    ftype = file.type
+    fsize = file.size / 1024
+
+    st.subheader(fname)
+    st.write(f"类型: {ftype}, 大小: {fsize: .2f} KB")
